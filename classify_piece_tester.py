457f29e7371c019782bc86ed5663ae3f713819a2
hash451722 <93071591+hash451722@users.noreply.github.com>
2023-02-13 16:29:55 +0900

    update

--- classify_piece_tester.py
+++ classify_piece_tester.py
@@ -17,7 +17,7 @@ class ClassifyPiece():
     def run(self, img:np.ndarray) -> list:
         '''
         マス目で切り出された81個の画像の駒種を分類する. onnx使用.
-        input  : (n, 3, 64, 64)  (batch_size, channels, height, width)
+        input  : (n, 3, 64, 64)  (batch_size, channels, height, width) (R, G, B)
         return : nマスの駒予測値(3文字で表現、頭文字は先手:b, 後手:w, 空きマスはemp (empty))
         '''
         ort_session = ort.InferenceSession(str(self.path_piece_onnx), providers=['CPUExecutionProvider'])
