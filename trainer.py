7a68a1a46e55335feec8081ce7e2550f11c32f92
hash451722 <93071591+hash451722@users.noreply.github.com>
2023-03-03 22:15:52 +0900

    update

--- trainer.py
+++ trainer.py
@@ -68,7 +68,7 @@ class Args():
     def __init__(self) -> None:
         self.batch_size = 32
         self.test_batch_size = 32
-        self.epochs = 10
+        self.epochs = 20
         self.lr = 1.0
         self.gamma = 0.7
         self.log_interval = 10
